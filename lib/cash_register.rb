a9ceb94c816c8bb01192d4586e8521472128cb22
github username <github email address>
2015-10-12 14:41:19 -0400

    added extra

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,14 +1,18 @@
 class CashRegister
-  attr_accessor :total, :discount, :items
+  attr_accessor :total, :discount, :items, :price
   def initialize(discount = 0)
     @total = 0
     @discount = discount
     @items = []
+    @price = []
   end
 
   def add_item(name, price, quant = 1)
     @total += (price * quant)
-    quant.times {|x| @items << name}
+    quant.times do |x|
+      @items << name
+      @price << price
+    end
   end
 
   def apply_discount
@@ -18,6 +22,11 @@ class CashRegister
     else
       return "After the discount, the total comes to $#{@total.to_i}."
     end
+  end
+
+  def void_last_transaction
+    @items.pop
+    @total -= @price.pop
   end
 
 end
